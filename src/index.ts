e1eb265d38f1c9af1c5bf3e344915faeef394d89
André Gonçalves <andre.goncalves@uphold.com>
2024-01-26 18:44:48 +0000

    Add allow to pass config on initialize

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export {
   Environments as TOPPER_ENVIRONMENTS,
   Events as TOPPER_EVENTS,
+  Themes as TOPPER_THEMES,
   Urls as TOPPER_URLS,
   Variants as TOPPER_VARIANTS
 } from './enums';
--- src/interfaces/config.ts
+++ src/interfaces/config.ts
@@ -1,10 +1,11 @@
 import { Environments } from '../enums/environments';
+import { Themes } from '../enums/themes';
 import { Variants } from '../enums/variants';
 
 export interface Config {
   environment?: Environments;
   is_android_app?: boolean;
   is_ios_app?: boolean;
-  theme?: string;
+  theme?: Themes;
   variant?: Variants;
 }
