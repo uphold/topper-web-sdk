0bae40187196f047bb691bc9a4149b344a483e86
André Geraldes <andre.geraldes@uphold.com>
2024-03-22 16:55:52 +0000

    Add locale support

--- src/interfaces/config.ts
+++ src/interfaces/config.ts
@@ -1,4 +1,5 @@
 import { Environments } from '../enums/environments';
+import { Locales } from '../enums/locales';
 import { Themes } from '../enums/themes';
 import { Variants } from '../enums/variants';
 
@@ -6,6 +7,7 @@ export interface Config {
   environment?: Environments;
   is_android_app?: boolean;
   is_ios_app?: boolean;
+  locale?: Locales;
   theme?: Themes;
   variant?: Variants;
 }
