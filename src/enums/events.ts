e27f9dfaf16ce95ef77bc97882c3567498805877
André Gonçalves <andre.goncalves@uphold.com>
2024-10-07 14:23:23 +0100

    Add offramp order wallet send event

--- src/enums/events.ts
+++ src/enums/events.ts
@@ -2,5 +2,6 @@ export enum Events {
   ALL = '*',
   ORDER_CONTINUE_BUTTON_CLICKED = 'orderContinueButtonClicked',
   ORDER_PLACED = 'orderPlaced',
+  ORDER_WALLET_SEND_BUTTON_CLICKED = 'orderWalletSendButtonClicked',
   WIDGET_CONTINUE_BUTTON_CLICKED = 'widgetContinueButtonClicked'
 }
