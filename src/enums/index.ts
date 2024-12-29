ee6e774576d9b8ef5098067ffce79fc4b2563f7d
André Gonçalves <andre.goncalves@uphold.com>
2024-12-29 21:20:28 +0000

    Add support for react native webview events

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -2,6 +2,7 @@ export * from './environments';
 export * from './events';
 export * from './flows';
 export * from './locales';
+export * from './targets';
 export * from './themes';
 export * from './urls';
 export * from './variants';
